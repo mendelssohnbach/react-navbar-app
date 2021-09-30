4bfa1a676e0701ffe3ae485f80a05e2d9b1d5ade
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-09-30 15:43:51 +0900

    :lipstick: スタイル調整
    https://gitmoji.dev/

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Music from './components/Music';
 import News from './components/News';
 import Sports from './components/Sports';
 import Trending from './components/Trending';
+import Navbar from './components/Navbar';
 
 export const App = () => {
   return (
     <>
+      <Navbar />
       <div className="flex justify-center items-center w-screen h-screen">
         <div className="w-11/12 h-5/6 md:w-9/12">
           <Switch>
